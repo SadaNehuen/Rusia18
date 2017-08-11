b547fb9fd20d9ccd893915ce4a31d6275fedb273
Gonzalo Zamudio <ahogadosderazon@gmail.com>
2017-08-11 03:08:45 -0300

    Modelos y migraciones iniciales. Falta ver en detalle Usuarios, Grupos y permisos

--- mundial/admin.py
+++ mundial/admin.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Grupo, Partido, Seleccion, Pronostico, Sede, Ronda, Resultado
 
 # Register your models here.
+admin.site.register(Grupo)
+admin.site.register(Seleccion)
+admin.site.register(Ronda)
+admin.site.register(Sede)
+admin.site.register(Pronostico)
+admin.site.register(Partido)
+admin.site.register(Resultado)
--- mundial/models.py
+++ mundial/models.py
@@ -4,3 +4,35 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Grupo(models.Model):
+    letra = models.CharField(max_length=1)
+
+class Seleccion(models.Model):
+    nombre = models.CharField(max_length=60)
+    img_url = models.CharField(max_length=200)
+    grupo = models.ForeignKey(Grupo, on_delete=models.CASCADE)
+
+class Sede(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Ronda(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Resultado(models.Model):
+    goles_local = models.IntegerField(default=None)
+    goles_visitante = models.IntegerField(default=None)
+    penales_local = models.IntegerField(default=None)
+    penales_visitante = models.IntegerField(default=None)
+    ganador_id = models.IntegerField(default=None)
+
+class Partido(models.Model):
+    fecha = models.DateTimeField()
+    sede = models.ForeignKey(Sede, on_delete=None)
+    ronda = models.ForeignKey(Ronda, on_delete=None)
+    resultado = models.ForeignKey(Resultado, on_delete=None)
+    jugado = models.BooleanField(default=False)
+
+class Pronostico(models.Model):
+    partido = models.ForeignKey(Partido, on_delete=None)
+    resultado = models.ForeignKey(Resultado, on_delete=None)
+    puntos = models.IntegerField(default=0)
